d71384cd41aec13df45db67cf3dfe271291722f5
jouider <jouider.abdellah@gmail.com>
2025-04-08 23:32:38 +0100

    Update Blog model with fillable fields and relationships

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
-        'title', 'detail'
+        'title',
+        'content',
+        'image',
+        'status',
     ];
 }
